342d4fc85ccb37d362b4a62f8211c9573b897291
Timothee Saisset <timothee.saisset@spacefill.fr>
2023-02-12 12:42:11 +0100

    feat: save state in local storage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,28 +1,38 @@
 <script>
 	import Repl from '$lib/index.svelte';
-	import { onMount } from 'svelte';
+	import { onMount, onDestroy } from 'svelte';
 	import '@sveltejs/site-kit/styles/index.css';
 	import '@sveltejs/site-kit/styles/code.css';
 
 	let repl;
+    let components = [
+        {
+            name: 'App',
+            type: 'svelte',
+            source: `<h1>Hello world !</h1>`
+        }
+    ]
+
+
+    function callback() {
+        if (components) localStorage.setItem('components', JSON.stringify(components))
+    }
+
+    function onInterval(callback, milliseconds) {
+        const interval = setInterval(callback, milliseconds);
+
+        onDestroy(() => {
+            clearInterval(interval)
+        })
+	};
+
+	onInterval(callback, 10000)
 
 	onMount(() => {
+        components = JSON.parse(localStorage.getItem('components')) || components
 		repl.set({
-			components: [
-				{
-					name: 'App',
-					type: 'svelte',
-					source:
-						`<scr` +
-						`ipt>
-	let name = 'world';
-</scr` +
-						`ipt>
-
-<h1>Hello {name}!</h1>`
-				}
-			]
-		});
+            components: components
+        });
 	});
 </script>
 
